caa0c251ea97b07ab9aee34b479f9b8096caf65f
sanjaypranav <sanjaypriya195@gmail.com>
2022-03-18 15:30:40 +0530

    updated Readme file

---
+++ FashionMnist.py
@@ -0,0 +1,44 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+
+
+train_dataloader = DataLoader(training_data, batch_size=60000, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=10000, shuffle=True)
+
+train_features, train_labels = next(iter(train_dataloader))
+test_features, test_labels = next(iter(test_dataloader))
+
+
+print('Training data size =',train_features.size(), len(train_labels))
+print('Testing data size',test_features.size(),len(train_labels))
--- Imnet.py
+++ Imnet.py
@@ -1,2 +1,27 @@
 import torch
-import seaborn as sns
+from torch import nn
+
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # Inputs to hidden layer linear transformation
+        self.hidden = nn.Linear(784, 256)
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(256, 10)
+        
+        # Define sigmoid activation and softmax output 
+        self.softmax = nn.Softmax()
+        
+    def forward(self, x, n_classes):
+        # Pass the input tensor through each of our operations
+        x = self.hidden(x)
+        x = nn.functional.relu(x) 
+        x = self.output(x)
+        x = self.softmax(x , n_classes)
+        
+        return x
+
+
+ 
